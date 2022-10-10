21af2655a62b3d60fe3eb79cbbd363db9fdaf37f
Johnson-MintyC <107607833+Johnson-MintyC@users.noreply.github.com>
2022-10-10 16:05:30 +1100

    player skeleton

--- tests/test_player.py
+++ tests/test_player.py
@@ -1,1 +1,18 @@
 import unittest
+from unittest.mock import MagicMock
+
+from poker.hand import Hand
+from poker.player import Player
+
+class PlayerTest(unittest.TestCase):
+    def test_stores_name_hand(self):
+        hand = Hand(cards = [])
+        player = Player(name = "Timmy", hand = hand)
+        self.assertEqual(player.name, "Timmy")
+        self.assertEqual(player.hand, hand)
+
+    def test_figures_out_own_best_hand(self):
+        mock_hand = MagicMock()
+        player = Player(name = "Jimmy", hand = mock_hand)
+
+        player.best_hand()
